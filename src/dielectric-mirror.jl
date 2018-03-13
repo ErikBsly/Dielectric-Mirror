eedd97b7b4f69203c39eb0cb439b6fe9bd12784b
Erik <11092915+ErikBsly@users.noreply.github.com>
2018-03-13 13:04:07 +0100

    Update dielectric-mirror.jl

--- src/dielectric-mirror.jl
+++ src/dielectric-mirror.jl
@@ -2,7 +2,7 @@
 # SiO2: https://refractiveindex.info/?shelf=main&book=Ta2O5&page=Bright-amorphous
 # Ta2O5: https://refractiveindex.info/?shelf=main&book=SiO2&page=Malitson
 
-function PeriodicDielectricMirror(λ::Float64, λ₀::Float64, θ_::Float64 = 0.0, N::Int = 20)
+function ReflectivityOfDielectricMirror(λ::Float64, λ₀::Float64, θ_::Float64 = 0.0, N::Int = 20)
 	θ = deg2rad(θ_)
 	n₀, n₁, n₂, ns = 1.0, 2.1411, 1.4599, 1.0 # Sorrounding(e.g. Ethylene Glyc.), Ta2O5, SiO2, Substrate
 	
@@ -24,6 +24,6 @@ end
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 λ_ = 250.0 : 0.5 : 700.0
-T = [1-PeriodicDielectricMirror(λ*1e-9, 550e-9) for λ in λ_] # T = 1 - R
+T = [1-ReflectivityOfDielectricMirror(λ*1e-9, 550e-9) for λ in λ_] # T = 1 - R
 
 writedlm("out.dat", zip(λ_, T), "\t")
